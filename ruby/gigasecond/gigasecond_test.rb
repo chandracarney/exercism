8dd71ed0b173e7fc2a3e5a4e416b9595a0b9e832
Chandra Carney <chandracarney@me.com>
2015-03-30 21:29:25 -0600

    Gigasecond tests passing

---
+++ ruby/gigasecond/gigasecond_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'date'
+require 'time'
+
+require_relative 'gigasecond'
+
+class GigasecondTest < MiniTest::Unit::TestCase
+  def test_1
+    gs = Gigasecond.from(Date.new(2011, 4, 25))
+    assert_equal Date.new(2043, 1, 1), gs
+  end
+
+  def test_2
+    gs = Gigasecond.from(Date.new(1977, 6, 13))
+    assert_equal Date.new(2009, 2, 19), gs
+  end
+
+  def test_3
+    gs = Gigasecond.from(Date.new(1959, 7, 19))
+    assert_equal Date.new(1991, 3, 27), gs
+  end
+
+  def test_time_with_seconds
+    gs = Gigasecond.from(Time.new(1959, 7, 19, 23, 59, 59))
+    assert_equal Date.new(1991, 3, 28), gs
+  end
+
+  def test_yourself
+    your_birthday = Date.new(1900, 1, 1)
+    gs = Gigasecond.from(your_birthday)
+    assert_equal Date.new(1931, 9, 10), gs
+  end
+end
