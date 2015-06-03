f6cac92544341ae4ea32f28a8f8b9dfbcf816027
Chandra Carney <chandracarney@me.com>
2015-06-03 10:27:57 -0600

    Roman numerals finished with tests passing.

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,16 @@
+class Fixnum
+  ROMAN = {
+    "M" => 1000, "CM" => 900, "D" => 500, "CD" => 400,
+    "C" => 100, "XC" => 90, "L" => 50, "XL" => 40,
+    "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1
+  }
+
+  def to_roman
+    number = self
+    roman = ""
+    ROMAN.map do |numeral, value|
+      roman << numeral && number -= value until number < value
+    end
+    roman
+  end
+end
